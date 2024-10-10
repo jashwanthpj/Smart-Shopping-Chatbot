2d176109f84b87b70b50cfa1d04b98bf46fc811e
jashwanthpj <podduturi.jashwanth@gmail.com>
2024-10-10 23:11:27 +0530

    - Filtered LLM responses to include only those with a match percentage of 85% or higher. - Structured product data with LLM responses into a JSON format for further processing.

--- main.py
+++ main.py
@@ -5,27 +5,30 @@ import os
 import json
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
-genai.configure(api_key=os.environ["API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
-# API_KEY = "AIzaSyA-mo1K9o-vYyb-Xtbrnde_E_oCaMupkVs"
-# API_URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}"
 
-user_query = "I want a black shirt, only cotton for men"
+API_KEY = "AIzaSyA-mo1K9o-vYyb-Xtbrnde_E_oCaMupkVs"
+genai.configure(api_key=API_KEY)
+llm_model = genai.GenerativeModel("gemini-1.5-flash")
+
+user_query = "I want a black tshirt for boys, pure cotton"
 
 query_embedding = model.encode(user_query).tolist()
 
 try:
     conn = psycopg2.connect(
         dbname = "shopping_data",
-        username = "postgres",
+        user = "postgres",
         password = "Pj262001@",
         host = "localhost"
     )
 
     cursor = conn.cursor()
 
-    cursor.execute(
-        f"SELECT id, category, sub_category, uri, content, pdt_desc as description FROM apparels ORDER BY embedding <=> {query_embedding}:: vector LIMIT 10"
+    cursor.execute("""
+        SELECT id, category, sub_category, uri, content, pdt_desc as description
+        FROM apparels
+        ORDER BY embedding <=> %s::vector LIMIT 10
+    """, (query_embedding,)
     )
 
     responses = cursor.fetchall()
@@ -40,7 +43,7 @@ try:
         pdt_desc = response[5]
 
         prompt = f"""Read this user search text: '{user_query}' 
-                    Compare it against the product record: '{response}' 
+                    Compare it against the product record: '{pdt_desc}' 
                     Return a response with 3 values:
                     1) MATCH: if the 2 products are at least 85% matching or not: YES or NO
                     2) PERCENTAGE: percentage of match, make sure that this percentage is accurate
@@ -48,25 +51,32 @@ try:
                     Remember if the user search text says that some attribute should not be there, and the product record has it, it should be a NO match.
                     """
         
-        LLM_output = genai.generate_text(model = "gemini-1.5-flash", prompt = prompt).text.strip()
-        print(LLM_output)
+        LLM_output = llm_model.generate_content(prompt).text.strip()
+        # print(LLM_output)
 
-        product_data = {
-            "id" : id,
-            "category" : category,
-            "sub_category" : sub_category,
-            "uri" : uri,
-            "description" : pdt_desc,
-            "llm_response" : LLM_output
-        }
+        if '**MATCH:** YES' in LLM_output:
+            product_data = {
+                "id" : id,
+                "category" : category,
+                "sub_category" : sub_category,
+                "uri" : uri,
+                "description" : pdt_desc,
+                "llm_response" : LLM_output
+            }
 
-        product_list.append(product_data)
+            product_list.append(product_data)
 
+    final_suggestions = product_list[:min(4,len(product_list))]
     json_output = json.dumps(product_list)
-    print(json_output)
+    print('total suggested products: ', len(product_list))
+    print('Final Suggestions: ', len(final_suggestions))
 
 except Exception as e:
     print("could not fetch data : ", e)
 
-
+finally:
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
 
